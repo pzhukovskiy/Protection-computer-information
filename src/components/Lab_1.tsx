c7ea99728e95f76322e4cc1aba214551a04428bb
Pavel Zhukovskiy <pavel20122004@mail.ru>
2023-03-01 19:16:56 +0300

    add basic adaptive design

---
+++ src/components/Lab_1.tsx
@@ -0,0 +1,7 @@
+function Lab_1() {
+    return (
+        <div className=""></div>
+    )
+}
+
+export default Lab_1;
---
+++ src/components/Mainpage.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@mui/material";
+import styles from "../styles/Mainpage.module.scss";
+
+
+function Mainpage() {
+    const list_lab = [
+        {id: 1, text: "Лабораторная работа №1"},
+        {id: 2, text: "Лабораторная работа №2"},
+        {id: 3, text: "Лабораторная работа №3"},
+        {id: 4, text: "Лабораторная работа №4"},
+        {id: 5, text: "Лабораторная работа №5"},
+        {id: 6, text: "Лабораторная работа №6"},
+        {id: 7, text: "Лабораторная работа №7"},
+        {id: 8, text: "Лабораторная работа №8"},
+        {id: 9, text: "Лабораторная работа №9"},
+        {id: 10, text: "Лабораторная работа №10"},
+        {id: 11, text: "Лабораторная работа №11"},
+    ]
+    return (
+        <div>
+            <div className={styles.header}>
+                <h1>Защита компьютерной информации</h1>
+            </div>
+            <div className={styles.choice}>
+                {list_lab.map((nameLab) => {
+                    return (
+                        <div key={nameLab.id} className={styles.choice_lab}>
+                            <h2>{nameLab.text}</h2>
+                            <Button 
+                                variant="outlined"
+                                onClick={() => console.log(nameLab.id)}>
+                                    Перейти
+                            </Button>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Mainpage;
